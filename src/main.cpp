3fdc8481045bf97788cdec926ba4de9d9520cf8e
Okorok01 <vladislav.v04@gmail.com>
2026-05-08 20:29:17 +0300

    b4 васильев владислав

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
-
+#include <gtest/gtest.h>
 #include "Automata.h"
 #include <iostream>
 
-int main() {
+int main((int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+
     Automata machine;
 
     std::cout << "=== Демонстрация работы автомата ===\n";
